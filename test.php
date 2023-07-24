eefa50055781fbbf98c086cb7d4874cfe61a2800
Rudradev <rudradev93@gmail.com>
2023-07-25 00:15:54 +0530

    red golden registration page

---
+++ test.php
@@ -0,0 +1,48 @@
+<!-- <html>
+<body>
+
+Welcome <?php echo $_POST["name"]; ?><br>
+Your email address is: <?php echo $_POST["email"]; ?>
+
+</body>
+</html> -->
+
+
+<!DOCTYPE html>
+<html>
+<head></head>
+<body>
+<?php
+// Database connection parameters.
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "SampleDB";
+$user=$_POST['uname'];  
+$pass=$_POST['psw']; 
+#echo $user.$pass; 
+
+$con=mysql_connect('localhost','root','') or die(mysql_error());  
+    mysql_select_db('SampleDB') or die("cannot select DB");
+#echo $con; 
+$query=mysql_query("SELECT * FROM sampletable WHERE username='".$user."' AND password='".$pass."'");  
+    $numrows=mysql_num_rows($query);  
+#echo $numrows;
+if($numrows!=0)  
+    {  
+    while($row=mysql_fetch_assoc($query))  
+    {  
+        $dbusername=$row['username'];  
+        $dbpassword=$row['password']; 
+    }
+    #echo $dbusername.$dbpassword;
+    if($user == $dbusername && $pass == $dbpassword)  
+    {  
+        echo "Valid User .. Successfully Logged In !!";  
+    } else {  
+        echo "Invalid Username or password!";  
+    } 
+    }
+?>
+</body>
+</html>
